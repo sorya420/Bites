ecc0282ca5e10079557500dc55b135e352aa5aad
Sonu kumar <soryasonu420@gmail.com>
2026-08-22 14:36:46 +0530

    attack frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://bites-frontend-phi.vercel.app",
     credentials:true
 }))
 app.use(cookieParser());
