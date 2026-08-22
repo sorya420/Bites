0c5feb99e8ff9a11dcccf639838b3ca080de67e6
Sonu kumar <soryasonu420@gmail.com>
2026-08-22 14:53:49 +0530

    update in serverjs

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const app = require('./src/app');
-const connectDB = require('./src/db/db')
+const connectDB = require('./src/db/db');
 
-connectDB()
+connectDB();
 
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
 
-app.listen(3000,()=> {
-    console.log("Server is runnig on port 3000");
-})
+module.exports = app;
