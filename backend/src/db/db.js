348d0b0ea6260424fb24c0f8927c046de6f0502a
Sonu kumar <soryasonu420@gmail.com>
2026-08-22 18:06:27 +0530

    updateDB

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
+  console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+  console.log("MONGO_URI value (partial):", process.env.MONGO_URI?.slice(0, 20));
+  
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (err) {
     console.error("MongoDB connection error: ", err);
-    throw err; 
+    throw err;
   }
 }
 
