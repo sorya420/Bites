a2955abc25b59c5b5736a1366ba429c3f889fe2b
Sonu kumar <soryasonu420@gmail.com>
2026-08-22 20:36:23 +0530

    new fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FRONTEND_URL,
     credentials:true
 }))
 app.use(cookieParser());
